e8dc467a983b5c0fb0d7ce16e270343358f31fce
Toxa <suppertoha@yandex.ru>
2022-09-28 17:36:33 +0300

    'last'

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
-
 $(document).ready(function () {
-
   // ! Header__burger
 
-  $(".header__burger").on("click", function () {
-    $(this).toggleClass("active");
-    $(".header__mobile").toggleClass("active");
+  $('.header__burger').on('click', function () {
+    $(this).toggleClass('active');
+    $('.header__mobile').toggleClass('active');
   });
 
-  $(document).on("click", function (e) {
-    if (!$(e.target).closest(".header__burger, .header__mobile").length) {
-      $(".header__mobile").removeClass("active");
-      $(".header__burger").removeClass("active");
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest('.header__burger, .header__mobile').length) {
+      $('.header__mobile').removeClass('active');
+      $('.header__burger').removeClass('active');
     }
     e.stopPropagation();
   });
 
+  //!
 
+  jQuery(document).ready(function($) {
+    var tinyHead = function() {
+        $('header').toggleClass('tinyHead', $(document).scrollTop() > 0);
+    }
 
+    tinyHead();
 
+    $(window).on('scroll touchmove', tinyHead);
+  });
 });
